db9b652aa44de9e0f85cd59bed848725519d1da4
iqbaltech07 <dev.iqbal007@gmail.com>
2025-07-11 14:45:27 +0700

    update: style card, data products

--- src/data/products.ts
+++ src/data/products.ts
@@ -12,22 +12,24 @@ const products: Product[] = [
     name: "Wireless Headphones",
     description: "High quality wireless headphones with noise cancellation.",
     price: 129.99,
-    image: "https://via.placeholder.com/300x200?text=Headphones",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5968PicqgKEOmA9jPVyIGucUsNBZobqDdpw&s",
   },
   {
     id: 2,
     name: "Smartwatch",
     description: "Track your fitness and notifications on the go.",
     price: 89.5,
-    image: "https://via.placeholder.com/300x200?text=Smartwatch",
+    image:
+      "https://static.retailworldvn.com/Products/Images/12249/329366/smartwatch-xiaomi-smart-band-9-arctic-blue-thumb-600x600.jpg",
   },
   {
     id: 3,
     name: "Gaming Mouse",
     description: "Ergonomic mouse with customizable RGB lighting.",
     price: 45.0,
-    image: "https://via.placeholder.com/300x200?text=Gaming+Mouse",
+    image: "https://www.softcom.co.id/wp-content/uploads/2020/04/G-PRO-a-1.jpg",
   },
 ];
 
-export {products}
+export { products };
